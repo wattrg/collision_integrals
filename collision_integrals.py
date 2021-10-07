82133c027d40aeeba82c27232f7d9068361613f0
Robert Watt <robertwhat@hotmail.com>
2021-10-07 14:46:06 +1000

    plotting dimensionless collision integrals

--- collision_integrals.py
+++ collision_integrals.py
@@ -283,13 +283,18 @@ if __name__ == "__main__":
     hcb_co2_n2_11 = ci.construct_ci(ci_type="dimensionless",
                                     sigma=3.692, epsilon=154.26, mu=0.01712,
                                     l=1, s=1)
+    hcb_co2_n2_22 = ci.construct_ci(ci_type="dimensionless",
+                                    sigma=3.692, epsilon=154.26, mu=0.01712,
+                                    l=2, s=2)
     temps = np.linspace(300, 20000, 150)
     hcb_omega_co2_co2_11 = hcb_co2_co2_11.eval(temps)
     hcb_omega_co2_co2_22 = hcb_co2_co2_22.eval(temps)
     hcb_omega_co2_n2_11 = hcb_co2_n2_11.eval(temps)
+    hcb_omega_co2_n2_22 = hcb_co2_n2_22.eval(temps)
     wright_omega_11 = cf.get_cis(pair="CO2:CO2", ci_type="pi_Omega_11").eval(temps)
     wright_omega_22 = cf.get_cis(pair="CO2:CO2", ci_type="pi_Omega_22").eval(temps)
-    wright_omega_co2_n2_22 = cf.get_cis(pair="CO2:N2", ci_type="pi_Omega_11").eval(temps)
+    wright_omega_co2_n2_11 = cf.get_cis(pair="CO2:N2", ci_type="pi_Omega_11").eval(temps)
+    wright_omega_co2_n2_22 = cf.get_cis(pair="CO2:N2", ci_type="pi_Omega_22").eval(temps)
 
     fig, ax = plt.subplots(2, 2)
     ax[0, 0].plot(temps, hcb_omega_co2_co2_11, label="dimensionless")
@@ -303,9 +308,14 @@ if __name__ == "__main__":
     ax[0 ,1].set_ylabel("$\\Omega^{2,2}_{CO_2, CO_2}$")
     ax[0 ,1].legend()
     ax[1 ,0].plot(temps, hcb_omega_co2_n2_11, label="dimensionless")
-    ax[1 ,0].plot(temps, wright_omega_co2_n2_22, label="Wright et al")
+    ax[1 ,0].plot(temps, wright_omega_co2_n2_11, label="Wright et al")
     ax[1 ,0].set_xlabel("Temperature [K]")
-    ax[1 ,0].set_ylabel("$\\Omega^{2,2}_{CO_2, CO_2}$")
+    ax[1 ,0].set_ylabel("$\\Omega^{1,1}_{CO_2, N_2}$")
     ax[1 ,0].legend()
+    ax[1 ,1].plot(temps, hcb_omega_co2_n2_22, label="dimensionless")
+    ax[1 ,1].plot(temps, wright_omega_co2_n2_22, label="Wright et al")
+    ax[1 ,1].set_xlabel("Temperature [K]")
+    ax[1 ,1].set_ylabel("$\\Omega^{2,2}_{CO_2, N_2}$")
+    ax[1 ,1].legend()
 
     plt.show()
