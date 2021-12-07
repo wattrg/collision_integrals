e89ee3ea5f37a8e9194a16ab73404523733cc2ad
Robert Watt <robertwhat@hotmail.com>
2021-12-07 14:03:36 +1000

    match eilmer's data exactly

--- tests/air_gas_model.py
+++ tests/air_gas_model.py
@@ -1,7 +1,7 @@
 air = {
     "N2": {
         "type": "molecule",
-        "M": 2.8013e-2,
+        "M": 2.80134e-2,
         "sigma": 3.621,
         "epsilon": 97.53,
         "charge": 0,
@@ -15,21 +15,21 @@ air = {
     },
     "N": {
         "type": "atom",
-        "M": 1.4e-2,
+        "M": 1.40067e-2,
         "sigma": 3.298,
         "epsilon": 71.4,
         "charge": 0
     },
     "O": {
         "type": "atom",
-        "M": 1.599e-2,
+        "M": 1.59994e-2,
         "sigma": 2.75,
         "epsilon": 80,
         "charge": 0,
     },
     "NO": {
         "type": "molecule",
-        "M": 3e-2,
+        "M": 3.0006e-2,
         "sigma": 3.621,
         "epsilon": 97.53,
         "charge": 0,
